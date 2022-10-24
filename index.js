e6960455cfb315e50b5c1470bed7cd28d9c0ede0
Kingsley Anyichie <111355135+Kingston7777777@users.noreply.github.com>
2022-10-24 10:04:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
-let engTrans01 = ["They are the best teams", "They are the best teams", "The main event"];
-let engTrans11 = ["The Master", "The best", "The great teams", "The champions."];
-let engTrans21 = ["A big meeting", "A great sporting event", "The main event."];
-let engTrans31 = ["The Master", "The best", "The great teams", "The champions."];
-let engTrans41 = ["They are the best teams", "They are the best teams", "These are the champions."];
-let engTrans51 = ["The Master", "The best", "The champions."];
-let all = [engTrans01, engTrans11, engTrans21, engTrans31, engTrans41, engTrans51];
-let sub_Div = document.getElementById("sub_Div");
+let dP01 = "They are the best teams";
+let dP02 = "They are the best teams";
+let dP03 = "The main event";
+let p01 = document.getElementById("p01");
+let p02 = document.getElementById("p02");
+let p03 = document.getElementById("p03");
+
+let dP11 = "The master";
+let dP12 = "The best";
+let dP13 = "The great teams";
+let dP14 = "The champions";
+let p11 = document.getElementById("p11");
+let p12 = document.getElementById("p12");
+let p13 = document.getElementById("p13");
+let p14 = document.getElementById("p14");
+
+let dP21 = "A big meeting";
+let dP22 = "A great sporting event";
+let dP23 = "The main event";
+let p21 = document.getElementById("p21");
+let p22 = document.getElementById("p22");
+let p23 = document.getElementById("p23");
+
+let dP31 = "The master";
+let dP32 = "The best";
+let dP33 = "The great teams";
+let dP34 = "The champions";
+let p31 = document.getElementById("p31");
+let p32 = document.getElementById("p32");
+let p33 = document.getElementById("p33");
+let p34 = document.getElementById("p34");
+
+let dP41 = "They are the best teams";
+let dP42 = "They are the best teams";
+let dP43 = "These are the champions";
+let p41 = document.getElementById("p41");
+let p42 = document.getElementById("p42");
+let p43 = document.getElementById("p43");
+
+let dP51 = "The Master";
+let dP52 = "The best";
+let dP53 = "";
+let dP54 = "The champions";
+let p51 = document.getElementById("p51");
+let p52 = document.getElementById("p52");
+let p53 = document.getElementById("p53");
+let p54 = document.getElementById("p54");
+
 let translateBtn = document.getElementById("translateBtn");
 
 translateBtn.addEventListener("click", changeT);
 
 
 function changeT(){
-    sub_Div.innerHTML = all;
-    
+    p01.textContent = dP01;
+    p02.textContent = dP02;
+    p03.textContent = dP03;
+
+    p11.textContent = dP11;
+    p12.textContent = dP12;
+    p13.textContent = dP13;
+    p14.textContent = dP14;
+
+    p21.textContent = dP21;
+    p22.textContent = dP22;
+    p23.textContent = dP23;
+
+    p31.textContent = dP31;
+    p32.textContent = dP32;
+    p33.textContent = dP33;
+    p34.textContent = dP34;
+
+    p41.textContent = dP41;
+    p42.textContent = dP42;
+    p43.textContent = dP43;
+
+    p51.textContent = dP51;
+    p52.textContent = dP52;
+    p53.textContent = dP53;
+    p54.textContent = dP54;
 }
+
+
+
 
 
 
